abb67d6876fa4e7542000b4548687e0679a91ee0
Arhan <arhanh@nycstudents.net>
2022-10-28 15:14:03 -0400

    Completed

---
+++ src/Cat.java
@@ -0,0 +1,49 @@
+public class Cat
+{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    // add a getAge() getter below for
+    // obtaining the Cat's age
+
+
+
+    // MODIFY this method so that the Cat randomly
+    // returns one of three different messages
+    // (make any messages you want!) rather than always returning "meow".
+    // Each message should have equal chance of being returned.
+    public String speak() {
+        int optionsSpeak = (int) (Math.random() * 3) + 1;
+        if (optionsSpeak == 1)
+        {
+            return "meowww";
+        }
+        else if(optionsSpeak == 2)
+        {
+            return "prrrrr";
+        }
+        else
+        {
+            return "brrrr";
+        }
+    }
+
+    public void introduce() {
+        System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+    }
+}
